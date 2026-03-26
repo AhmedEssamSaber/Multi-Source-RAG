3675f3de53c9d42f07038e51acc85236f7f663d8
Ahmed Essam <ahmedessamsaperr156@gmail.com>
2026-03-26 10:47:16 +0200

    the main file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from app.controllers.chat_controller import router
 
+from app.models.db.database import init_db
+
 app = FastAPI(title="Multi Source Agentic RAG")
 
 app.include_router(router)
+
+
+@app.on_event("startup")
+async def startup():
+    print("🚀 STARTUP EVENT CALLED")
+    await init_db()

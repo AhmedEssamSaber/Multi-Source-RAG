02473cba7a1751c8c20c69a291560e46d720f708
Ahmed Essam <ahmedessamsaperr156@gmail.com>
2026-05-07 09:46:47 +0300

    database

--- app/models/db/database.py
+++ app/models/db/database.py
@@ -1,16 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from app.models.db.base import Base
-import os
+from app.core.config import settings
 
-import app.models.db.models
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://postgres:ahmed1234@localhost:5432/postgres"
-)
+import app.models.db.models  
 
 engine = create_async_engine(
-    DATABASE_URL,
+    settings.DATABASE_URL,
     echo=True,
     future=True
 )
@@ -24,13 +20,14 @@ AsyncSessionLocal = async_sessionmaker(
 async def init_db():
     print("INIT DB CALLED")
 
+    from app.models.db.base import Base
+    print("TABLES:", Base.metadata.tables.keys())  
+
     async with engine.begin() as conn:
-        print("METADATA TABLES:", Base.metadata.tables.keys())
         await conn.run_sync(Base.metadata.create_all)
 
     print("DONE")
 
-        
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
